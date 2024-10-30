b62886ac50a4c55e0b9dcfca538f9f899b8f2db5
Xu Feiyang <3316560771@qq.com>
2024-10-30 15:29:13 +0800

    feat：支持代理商设置二级域名

--- model/entity.go
+++ model/entity.go
@@ -10,7 +10,6 @@ type User struct {
 	LastLoginTime    time.Time `json:"last_login_time"`   // 上一次登录时间
 	Role             string    `json:"role"`              // 用户角色，可以为空
 	AgentId          int       `json:"agent_id"`
-	SubDomain        string    `json:"sub_domain"` // 代理商二级域名前缀，如zhangsan.mixpaper.cn，前缀为zhangsan
 }
 
 type Agent struct {
